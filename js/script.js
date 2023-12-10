e260d92d4dc8be7d9a13bd0c1ba20abc52023805
Chayun Fadila <128894491+chayunfdl@users.noreply.github.com>
2023-12-11 04:26:13 +0700

    Integrate API

--- js/script.js
+++ js/script.js
@@ -19,9 +19,6 @@ function openTabs(evt, tabName) {
   }
 
 /*slide show*/
-  let slideIndex = 0;
-showSlides();
-
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
@@ -34,42 +31,55 @@ function showSlides() {
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 
-// Fetch all orders
-fetch(API_URL + '/orders')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Failed to fetch orders');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log('All orders:', data.data);
-    // Handle the data as needed
-  })
-  .catch(error => console.error('Error fetching orders:', error.message));
+const getAllMenu = () => {
+  // Fetch all orders
+  fetch(API_URL + '/orders')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch orders');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('All orders:', data.data);
+      // Handle the data as needed
+    })
+    .catch(error => console.error('Error fetching orders:', error.message));
+}
 
-// Add an order
-const orderDetails = {
-  name: 'John Doe',
-  email: 'john@example.com',
-  order: 'Black Coffee',
-};
+$("#order").on('click', () => {
+  const _confirm = confirm('Terimakasih kami akan mengirimkan email kepada anda ketika pesanan sudah siap');
 
-fetch(API_URL + '/addOrder', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(orderDetails),
+  if (!_confirm) return;
+
+  const name = $("input[name=name]").val()
+  const email = $("input[name=email]").val()
+  const order = $("input[name=order]").val()
+
+  // Add an order
+  const orderDetails = {
+    name,
+    email,
+    order,
+  };
+
+  fetch(API_URL + '/orders/add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(orderDetails),
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to place order');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Order placed successfully:', data);
+      // Handle the data as needed
+      alert("Order placed successfully")
+    })
+    .catch(error => console.error('Error placing order:', error.message));
 })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Failed to place order');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log('Order placed successfully:', data);
-    // Handle the data as needed
-  })
-  .catch(error => console.error('Error placing order:', error.message));
